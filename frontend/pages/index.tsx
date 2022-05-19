4d848bb93bf648b0e3acc4bad3df404353058128
diegobaena89 <diegobf.cello@gmail.com>
2022-05-19 20:11:06 -0300

    <feat> recovering token in SSR

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,30 +1,56 @@
-import { sign } from 'crypto'
-import { FormEvent, useContext, useState } from 'react'
-import { AuthContext } from '../contexts/AuthContext'
-import styles from '../styles/Home.module.css'
+import { GetServerSideProps } from "next";
+import { FormEvent, useContext, useEffect, useState } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import styles from "../styles/Home.module.css";
+import { parseCookies } from 'nookies'
 
 export default function Home() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const { signIn } = useContext(AuthContext)
+  const { signIn } = useContext(AuthContext);
 
   async function handleSubmit(event: FormEvent) {
-    event.preventDefault()
+    event.preventDefault();
 
     const data = {
       email,
-      password
-    }
+      password,
+    };
 
-    await signIn(data)
+    await signIn(data);
   }
 
   return (
     <form onSubmit={handleSubmit} className={styles.container}>
-      <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-      <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button type="submit">Entrar</button>
     </form>
-  )
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const cookies = parseCookies(ctx);
+
+  if(cookies['nextauth.token']) {
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
 }
